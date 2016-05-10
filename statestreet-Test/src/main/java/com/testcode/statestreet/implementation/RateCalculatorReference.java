7780a639b8e92c92853206f893ad191722b40e5f
Diego Casás Rodríguez <dcasasrodriguez@mdsol.com>
2016-05-10 12:38:36 +0100

    Update RateCalculatorReference.java

--- statestreet-Test/src/main/java/com/testcode/statestreet/implementation/RateCalculatorReference.java
+++ statestreet-Test/src/main/java/com/testcode/statestreet/implementation/RateCalculatorReference.java
@@ -10,30 +10,42 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-/**
- * Created by mateusdasilva on 08/05/2016.
- */
+
 public class RateCalculatorReference implements ReferenceRateCalculator {
 
+    //There are approx. 50 different price sources and 30 different price providers.
+    // The number of markets in the configuration varies from 3 to 15.
+    // --> meaning a small number of data so safe to keep in memory
     private Set<Market>marketList;
 
-
-
+    //onConfiguration() is called to pass the list of markets we want to calculate the reference rate across.
+    //onConfiguration() may be called again, which will be followed by onFxPrice() and calculate() calls.
+    //There is a separate instance of ReferenceRateCalculator for each currency pair.
+    //Every instance of ReferenceRateCalculator runs on a single thread, which means no synchronization required.
+    // --> meaning no synchronization required
     public void onConfiguration(Configuration configuration) {
-
+        //The configuration gets changed infrequently, 1 or 2 times per day.
+        // --> meaning this method can be heavy and we should do as much as possible here in order to do less on onFxPrice() and calculate()
         int sizeOfMarket = configuration.getSize();
         marketList = new HashSet<Market>(sizeOfMarket);
 
         for ( int listOfMarket =0; listOfMarket < sizeOfMarket; listOfMarket ++){
             PriceSource source = configuration.getSource(listOfMarket);
             PriceProvider provider = configuration.getProvider(listOfMarket);
+            //There are approx. 50 different price sources and 30 different price providers.
+            // The number of markets in the configuration varies from 3 to 15.
+            // --> there are duplicates and Market should implement equals() & hashcode() accordingly to avoid duplicates on the Set
             Market market = new Market(source, provider);
             marketList.add(market);
 
         }
     }
 
+
     public void onFxPrice(FxPrice fxPrice) {
+        //onFxPrice() is called on each market data update. By calling getSource() and getProvider() you know there the price came from. The price can be stale, in which case bid and offer are not defined (may be NaN or something else).
+        //Each onFxPrice() call is followed by calculate() call, which produces the reference rate. Bid and offer on the returning price are always set to the same value - the reference rate.
+        // --> think about what you would need to calculate the reference rate and see how you can do it here!
         Map<Market, FxPrice> priceByMarket = new HashMap();
 
         Market market = new Market(fxPrice.getSource(),fxPrice.getProvider());
@@ -50,6 +62,15 @@ public class RateCalculatorReference implements ReferenceRateCalculator {
     }
 
     public FxPrice calculate() {
+        //The rate gets recalculated after each market data update, thousands of ticks per second.
+        // --> Performance really important!
+        // --> think about what could be a minimal implementation. for example, how you can get to this possible implementation?
+        //
+        //   FxPrize referencePrize;
+        //   public FxPrice calculate() {
+        //       return referencePrize; // and make the calculations in onFxPrice() directly
+        //   }
+        
         //TODO: Here where we calculate the median price. how to?
 
 
